32dbf2485b6829f4c0a2d66a320eb5c7247b9fd3
didakusmaa <dindakusumaa57@gmail.com>
2023-08-10 08:55:27 +0700

    commit2

--- lib/app/widgets/splash.dart
+++ lib/app/widgets/splash.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class  SplashScreen extends StatelessWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
@@ -9,12 +9,23 @@ class  SplashScreen extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue[50],
-        body: Center(
-          child: Container(
-            width: Get.width * 0.5,
-            height: Get.width * 0.5,
-            child: Image.asset("assets/logokabelfix.png"),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.blue,
+                Colors.white,
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Center(
+            child: Container(
+              width: Get.width * 0.5,
+              height: Get.width * 0.5,
+              child: Image.asset("assets/logokabelfix.png"),
+            ),
           ),
         ),
       ),
